238cfc2999b0a6e76d8d2be01764f2bde770b88a
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-06-08 18:37:27 +0700

    add: authtoken middleware

--- src/routes/kelas.route.js
+++ src/routes/kelas.route.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 
+const { verifyToken } = require("../middlewares/auth");
 const { index } = require("../controllers/kelas.controller");
 
-router.get("/", index);
+router.get("/", verifyToken, index);
 
 module.exports = router;
--- src/routes/matapelajaran.route.js
+++ src/routes/matapelajaran.route.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 
+const { verifyToken } = require("../middlewares/auth");
 const { index } = require("../controllers/matapelajaran.controller");
 
-router.get("/:id_mode", index);
+router.get("/:id_mode", verifyToken, index);
 
 module.exports = router;
